dda2c6f32e081aa05eda3c42e23cfcf59a2ceb98
dashun <liuliu18@126.com>
2018-06-16 23:59:32 +0800

    change file

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -6,18 +6,32 @@ Page({
   data: {
     dataList: [
         {name: 'data 0组', url: '../data_0/list'},
-        {name: 'data 1组', url: '../data_1/list'},
         {name: 'data 10组', url: '../data_10/list'},
-        {name: 'data 100组', url: '../data_100/list'},
-        {name: 'data 200组', url: '../data_200/list'},
         {name: 'data 400组', url: '../data_400/list'},
-        {name: 'data 1000组', url: '../data_1000/list'}
-    ],
-    complexDataList: [
+        {name: 'data 1000组', url: '../data_1000/list'},
         {name: 'complex data 1000组', url: '../data_complex_1000/list'},
+        {name: 'other data 1000组', url: '../data_other_1000/list'},
+        {name: 'nest data 1000组', url: '../data_nest_1000/list'},
+        {name: 'set data 1000组', url: '../set_data_1000/list'},
+        {name: 'complex set data 1000', url: '../set_data_complex_1000/list'},
+        {name: 'nest set data 1000', url: '../set_data_nest_1000/list'},
+        {name: 'other set data 1000组', url: '../set_data_other_1000/list'},
+        {name: 'other set data nest 1000组', url: '../set_data_other_nest_1000/list'},
+        {name: 'other set data complex 1000组', url: '../set_data_other_complex_1000/list'}
+    ],
+    pieceDataList: [
+        {name: 'set data 1000组', url: '../set_data_1000/list'},
+        {name: 'piece set data whole 1000/10', url: '../piece_set_data_continue_whole_1000_10/list'},
+        {name: 'piece set data part 1000/10', url: '../piece_set_data_continue_part_1000_10/list'}
     ],
       complexDataSetDataList: [
-          {name: 'complex data Set Data 1000组', url: '../data_set_data_complex_1000/list'},
+          {name: 'complex data 1000 Set Data 1000', url: '../data_set_data_complex_1000_1000/list'},
+          {name: 'complex data 1000 Set Data other 0', url: '../data_set_data_other_complex_1000_0/list'},
+          {name: 'complex data 1000 Set Data other 1000', url: '../data_set_data_other_complex_1000_1000/list'}
+      ],
+      changeSetDataList: [
+          {name: 'complex set data change nokey 0_400_1000', url: '../set_data_complex_change_nokey_0_400_1000/list'},
+          {name: 'complex set data change key 0_400_1000', url: '../set_data_complex_change_key_0_400_1000/list'}
       ]
   },
   //事件处理函数
